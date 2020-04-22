700d22c6048bf67f7f941adc48d12cadc68360a3
huangyongjing <huangyongjing@kedacom.com>
2020-04-22 16:19:33 +0800

    #202004221619 责任链模式

---
+++ src/chainmodel/LiabilityChain.java
@@ -0,0 +1,19 @@
+package chainmodel;
+
+/**
+ * @author: mirror
+ * @date: 2020/4/22 16:13
+ * @description:
+ */
+public class LiabilityChain {
+
+    public void runChain() {
+
+        Task task3 = new Task3();
+        Task task2 = new Task2(task3);
+        Task task1 = new Task1(task2);
+        task1.run();
+
+    }
+
+}
---
+++ src/chainmodel/Task.java
@@ -0,0 +1,12 @@
+package chainmodel;
+
+/**
+ * @author: mirror
+ * @date: 2020/4/22 16:06
+ * @description:
+ */
+public interface Task {
+
+    void run();
+
+}
