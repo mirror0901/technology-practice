f262e6343e8e4e266121f1b53b8b702b6ccfc07d
mirror0901 <1755496180@qq.com>
2020-05-17 02:10:11 +0800

    #2020050170208 domain: 华为机试题1 name: flink从各种数据源读取数据 reference: https://www.nowcoder.com/practice/f9c6f980eeec43ef85be20755ddbeaf4?tpId=37&tqId=21239&rp=0&ru=/ta/huawei&qru=/ta/huawei/question-ranking

---
+++ src/huawei/Main005.java
@@ -0,0 +1,21 @@
+package huawei;
+
+import java.util.Scanner;
+
+/**
+ * @author: mirror_huang
+ * @qq: 1755496180
+ * @description: 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
+ * @create: 2020-05-16 21:47
+ **/
+public class Main005 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.next().substring(2);
+            System.out.println(Integer.parseInt(str, 16));
+        }
+    }
+
+}
