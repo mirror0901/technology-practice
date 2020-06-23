a9dbd77f12fd883318f600abc6ec56bf113212d8
huangyongjing <huangyongjing@kedacom.com>
2020-06-23 17:42:21 +0800

    #202006231741 domain: websocket description: https://blog.csdn.net/moshowgame/article/details/80275084?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase

---
+++ websocket/src/main/java/com/mirror/websocket/endpoint/WebSocketServer.java
@@ -0,0 +1,15 @@
+package com.mirror.websocket.endpoint;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author: mirror
+ * @date: 2020/6/23 11:23
+ * @description:
+ */
+@ServerEndpoint()
+public class WebSocketServer {
+
+
+
+}
